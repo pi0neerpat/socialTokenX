9f4dc8c1906d301e6f085fc310d89b895e2db583
Patrick <blockchainbuddha@gmail.com>
2020-12-02 16:52:26 -0500

    Native Super Token

--- contracts/contracts/SocialTokenX.sol
+++ contracts/contracts/SocialTokenX.sol
@@ -1,91 +1,71 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
 
 import {
-    ISuperfluid,
-    ISuperToken,
-    ISuperApp,
-    ISuperAgreement,
-    SuperAppDefinitions
-} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+  ISuperfluid,
+  ISuperToken,
+  ISuperApp,
+  SuperAppDefinitions,
+  ISuperAgreement } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
 
-contract SocialTokenX is ERC20, ISuperApp {
+contract Emanator is Context, ERC20, ISuperApp {
   using SafeMath for uint256;
 
-  ISuperfluid private host;
-  IConstantFlowAgreementV1 private cfa;
-  IInstantDistributionAgreementV1 private ida;
-  ISuperToken public tokenX;
+  address payable public creator;
 
-  constructor(ISuperfluid _host, IConstantFlowAgreementV1 _cfa, IInstantDistributionAgreementV1 _ida, ISuperToken _tokenX) ERC20 ("SocialTokenX", "STX") {
-      assert(address(_host) != address(0));
-      assert(address(_cfa) != address(0));
-      assert(address(_ida) != address(0));
-      assert(address(_tokenX) != address(0));
-      host = _host;
-      cfa = _cfa;
-      ida = _ida;
-      tokenX = _tokenX;
+  ISuperfluid private _host;
+  IConstantFlowAgreementV1 private _cfa;
+  ISuperToken private _acceptedToken;
+
+  constructor(
+      ISuperfluid host,
+      IConstantFlowAgreementV1 cfa,
+      ISuperToken acceptedToken
+      )
+      ERC20 ("SocialTokenX", "STX")
+      {
+      assert(address(host) != address(0));
+      assert(address(cfa) != address(0));
+      assert(address(acceptedToken) != address(0));
+
+      _host = host;
+      _cfa = cfa;
+      _acceptedToken = acceptedToken;
 
       uint256 configWord =
-            SuperAppDefinitions.TYPE_APP_FINAL |
-            // SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
-            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
-            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+          SuperAppDefinitions.TYPE_APP_FINAL;
+          // SuperAppDefinitions.TYPE_APP_FINAL |
+          // SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+          // SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+          // SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
 
-      host.registerApp(configWord);
+      _host.registerApp(configWord);
   }
 
-    function beforeAgreementCreated(
-        ISuperToken _tokenX,
-        bytes calldata _ctx,
-        address _agreementClass,
-        bytes32 agreementId,
-        bytes calldata /*cbdata*/
-    )
-        external
-        onlyExpected(_tokenX, _agreementClass)
-        onlyHost
-        returns (bytes memory)
-    {
-        // return _bid(_ctx, _tokenX, agreementId);
-    }
+/**************************************************************************
+ * SuperApp callbacks
+*************************************************************************/
 
-    function afterAgreementCreated(
-        ISuperToken _tokenX,
-        bytes calldata _ctx,
-        address _agreementClass,
-        bytes32 agreementId,
-        bytes calldata /*cbdata*/
-    )
-        external override
-        onlyExpected(_tokenX, _agreementClass)
-        onlyHost
-        returns (bytes memory)
-    {
-        // return _bid(_ctx, _tokenX, agreementId);
-    }
+     function beforeAgreementCreated(
+         ISuperToken /*superToken*/,
+         bytes calldata /*ctx*/,
+         address /*agreementClass*/,
+         bytes32 /*agreementId*/
+     )
+         external
+         view
+         virtual
+         override
+         returns (bytes memory /*cbdata*/)
+         {
+             revert("Unsupported callback - Before Agreement Created");
+         }
 
-    function beforeAgreementUpdated(
-      ISuperToken /*superToken*/,
-      bytes calldata /*ctx*/,
-      address /*agreementClass*/,
-      bytes32 /*agreementId*/
-    )
-    external
-    view
-    virtual
-    override
-    returns (bytes memory /*cbdata*/)
-    {
-      // revert("Unsupported callback - Before Agreement updated");
-    }
-
-    function afterAgreementUpdated(
+      function afterAgreementCreated(
         ISuperToken _superToken,
         bytes calldata _ctx,
         address _agreementClass,
@@ -97,7 +77,36 @@ contract SocialTokenX is ERC20, ISuperApp {
         onlyHost
         returns (bytes memory)
     {
-        // return _doExchange(_ctx, _superToken, agreementId);
+        revert("Unsupported callback - After Agreement Created");
+    }
+
+    function beforeAgreementUpdated(
+        ISuperToken /*superToken*/,
+        bytes calldata /*ctx*/,
+        address /*agreementClass*/,
+        bytes32 /*agreementId*/
+    )
+        external
+        view
+        virtual
+        override
+        returns (bytes memory /*cbdata*/)
+    {
+        revert("Unsupported callback - Before Agreement updated");
+    }
+    function afterAgreementUpdated(
+        ISuperToken _superToken,
+        bytes calldata _ctx,
+        address _agreementClass,
+        bytes32 agreementId,
+        bytes calldata /*cbdata*/
+    )
+        external override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory /*cbdata*/)
+    {
+        revert("Unsupported callback - After Agreement updated");
     }
 
     function beforeAgreementTerminated(
@@ -112,7 +121,7 @@ contract SocialTokenX is ERC20, ISuperApp {
         override
         returns (bytes memory /*cbdata*/)
     {
-        // revert("Unsupported callback -  Before Agreement Terminated");
+        revert("Unsupported callback -  Before Agreement Terminated");
     }
 
     function afterAgreementTerminated(
@@ -124,17 +133,13 @@ contract SocialTokenX is ERC20, ISuperApp {
     )
         external override
         onlyHost
-        returns (bytes memory)
+          returns (bytes memory /*cbdata*/)
     {
-        // According to the app basic law, we should never revert in a termination callback
-        //if (!_isAccepted(_superToken) || !_isCFAv1(_agreementClass)) return _ctx;
-        // return _stopExchange(_ctx, _superToken, _agreementId);
+        revert("Unsupported callback - After Agreement Terminated");
     }
 
-
-    // utilities
-    function _isAccepted(ISuperToken _tokenX) private view returns (bool) {
-        return address(_tokenX) == address(tokenX);
+    function _isAcceptedToken(ISuperToken _superToken) private view returns (bool) {
+        return address(_superToken) == address(_acceptedToken);
     }
 
     function _isCFAv1(address _agreementClass) private pure returns (bool) {
@@ -143,14 +148,13 @@ contract SocialTokenX is ERC20, ISuperApp {
     }
 
     modifier onlyHost() {
-        require(msg.sender == address(host), "RedirectAll: support only one host");
+        require(msg.sender == address(_host), "Auction: supports only one host");
         _;
     }
 
-    modifier onlyExpected(ISuperToken _tokenX, address _agreementClass) {
-        require(_isAccepted(_tokenX) , "Exchange: not accepted token");
-        require(_isCFAv1(_agreementClass), "Exchange: only CFAv1 supported");
+    modifier onlyExpected(ISuperToken _superToken, address _agreementClass) {
+        require(_isAcceptedToken(_superToken), "Auction: not accepted token");
+        require(_isCFAv1(_agreementClass), "Auction: only CFAv1 supported");
         _;
     }
-
  }
